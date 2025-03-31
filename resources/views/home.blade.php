5ae72cbfcd37ed0dead64b1b46ac8e4393e69b3b
SebasMen <sebastian.meneses@antpack.com>
2025-03-31 12:35:29 -0500

    add: layout, components and connection db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 12</title>
-</head>
-<body>
-    <h1>Bienvenido al Home con blade</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Home -')
+
+@push('css')
+    <style>
+        body {
+            background-color: #FDFDFC;
+        }
+    </style>
+@endpush
+
+@section('content')
+    <div class="max-w-4xl mx-auto px-4">
+        <h1 class="mb-4">Bienvenido al Home con blade</h1>
+
+        <x-alert
+            type="success"
+            title="Titulo de alerta"
+            class="mb-4"
+        >
+            {{-- <x-slot name="title">Titulo de alerta</x-slot> --}}
+
+            Esto es un alerta
+        </x-alert>
+
+        <p>Esto es un parrafo</p>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title' ) Laravel 12</title>
+
+    @stack('css')
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
